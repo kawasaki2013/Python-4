e8f6f524dfe83f1370b10d0636c0fdd1a91d36a8
JayedHossainJibon <jayed.swe@gmail.com>
2017-03-23 05:36:13 -0700

    Add files via upload
    CGPA

---
+++ CGPA.py
@@ -0,0 +1,43 @@
+# Now write a program to Academic Result of  diu
+
+print('Jibon')
+
+print('01987132107')
+
+print('jayed.swe@gmail.com')
+
+mark = int (input('Enter your mark :'))
+
+if mark >= 80 and mark <=100:
+    print('A + Grade ')
+
+elif mark >=75 and mark <=79:
+    print(' A Grade ')
+
+elif mark >=70 and mark <=74:
+    print(' A- Grade')
+
+elif mark >=65 and mark <=69:
+    print(' B+ Grade')
+
+elif mark >=60 and mark <=64:
+    print(' B Grade')
+
+elif mark >=55 and mark <=59:
+    print(' B- Grade')
+
+elif mark >=50 and mark <=54:
+    print(' C+ Grade ')
+
+elif mark >=45 and mark <=49:
+    print(' C Grade')
+
+elif mark >=40 and mark <=44:
+    print(' D Grade')
+
+elif mark >=00 and mark <=39:
+    print('Fail try again')
+
+else:
+    print('\nNumber in not in range')
+
